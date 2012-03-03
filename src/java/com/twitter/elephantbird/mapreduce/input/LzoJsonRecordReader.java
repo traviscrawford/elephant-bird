a28c5d03d5c12370c7e5812b085e68f350595b43
Travis Crawford <travis@twitter.com>
2012-03-02 21:40:04 -0800

    Turn LzoJsonRecordReader into a wrapper around LzoLineRecordReader to reduce duplicate code. This is a step towards a generic JsonInputFormat that could be used by JsonLoader to further simplify things, and ensure they behave in similar ways.

--- src/java/com/twitter/elephantbird/mapreduce/input/LzoJsonRecordReader.java
+++ src/java/com/twitter/elephantbird/mapreduce/input/LzoJsonRecordReader.java
@@ -1,13 +1,13 @@
 package com.twitter.elephantbird.mapreduce.input;
 
 import java.io.IOException;
-import java.io.InputStream;
 
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.MapWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.util.LineReader;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -15,74 +15,52 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Reads line from an lzo compressed text file, and decodes each line into a json map.
- * Skips lines that are invalid json.
- *
- * WARNING: Does not handle multi-line json input well, if at all.
- * TODO: Fix that, and keep Hadoop counters for invalid vs. valid lines.
+ * Decodes each line from an LZO compressed text file into a JSON map.
+ * Skips lines that are invalid JSON. Multi-line JSON records are
+ * not supported at this time.
  */
-public class LzoJsonRecordReader extends LzoRecordReader<LongWritable, MapWritable> {
+public class LzoJsonRecordReader extends RecordReader<LongWritable, MapWritable> {
   private static final Logger LOG = LoggerFactory.getLogger(LzoJsonRecordReader.class);
 
-  private LineReader in_;
+  private final LzoLineRecordReader reader = new LzoLineRecordReader();
+  private final LongWritable key = new LongWritable();
+  private final MapWritable value = new MapWritable();
+  private final JSONParser parser = new JSONParser();
 
-  private final LongWritable key_ = new LongWritable();
-  private final Text currentLine_ = new Text();
-  private final MapWritable value_ = new MapWritable();
-  private final JSONParser jsonParser_ = new JSONParser();
+  @Override
+  public void initialize(InputSplit inputSplit, TaskAttemptContext taskAttemptContext)
+      throws IOException, InterruptedException {
+    reader.initialize(inputSplit, taskAttemptContext);
+  }
 
   @Override
   public synchronized void close() throws IOException {
-    if (in_ != null) {
-      in_.close();
-    }
+    reader.close();
   }
 
   @Override
   public LongWritable getCurrentKey() throws IOException, InterruptedException {
-    return key_;
+    return key;
   }
 
   @Override
   public MapWritable getCurrentValue() throws IOException, InterruptedException {
-    return value_;
+    return value;
   }
 
   @Override
-  protected void createInputReader(InputStream input, Configuration conf) throws IOException {
-    in_ = new LineReader(input, conf);
-  }
-
-  @Override
-  protected void skipToNextSyncPoint(boolean atFirstRecord) throws IOException {
-    if (!atFirstRecord) {
-      in_.readLine(new Text());
-    }
+  public float getProgress() throws IOException, InterruptedException {
+    return reader.getProgress();
   }
 
   @Override
   public boolean nextKeyValue() throws IOException, InterruptedException {
-    // Since the lzop codec reads everything in lzo blocks, we can't stop if pos == end.
-    // Instead we wait for the next block to be read in, when pos will be > end.
-    value_.clear();
-
-    while (pos_ <= end_) {
-      key_.set(pos_);
-
-      int newSize = in_.readLine(currentLine_);
-      if (newSize == 0) {
-        return false;
+    while (reader.nextKeyValue()) {
+      // TODO(travis): Add success/failure counters
+      if (decodeLineToJson(parser, reader.getCurrentValue(), value)) {
+        return true;
       }
-
-      pos_ = getLzoFilePos();
-
-      if (!decodeLineToJson(jsonParser_, currentLine_, value_)) {
-        continue;
-      }
-
-      return true;
     }
-
     return false;
   }
 
@@ -117,4 +95,3 @@ public class LzoJsonRecordReader extends LzoRecordReader<LongWritable, MapWritab
     }
   }
 }
-
